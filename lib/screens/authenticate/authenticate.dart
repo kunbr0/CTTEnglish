27b15845b0e269bc8ded13790fd944a1372fe886
kunbr0 <toantanqn@gmail.com>
2020-09-13 12:02:07 +0700

    add precachePicture and FutureBuilder in Authenticate page

--- lib/screens/authenticate/authenticate.dart
+++ lib/screens/authenticate/authenticate.dart
@@ -1,9 +1,6 @@
-//import 'package:cttenglish/screens/authenticate/register.dart';
-//import 'package:cttenglish/screens/authenticate/sign_in.dart';
-import 'package:flutter/material.dart';
-import 'package:cttenglish/screens/auth/Screens/Welcome/welcome_screen.dart';
-import 'package:cttenglish/screens/auth/constants.dart';
-
+// import 'package:flutter/material.dart';
+// import 'package:cttenglish/screens/authenticate/register.dart';
+// import 'package:cttenglish/screens/authenticate/sign_in.dart';
 
 // class Authenticate extends StatefulWidget {
 //   @override
@@ -28,18 +25,61 @@ import 'package:cttenglish/screens/auth/constants.dart';
 //   }
 // }
 
-class Authenticate extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:cttenglish/screens/auth/Screens/Welcome/welcome_screen.dart';
+import 'package:cttenglish/screens/auth/constants.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class Authenticate extends StatefulWidget {
+  @override
+  _AuthenticateState createState() => _AuthenticateState();
+}
+
+class _AuthenticateState extends State<Authenticate> {
   // This widget is the root of your application.
+
+  // void initState() {
+  //   loadPictures();
+  //   super.initState();
+  // }
+
+  Future<bool> loadPictures() async {
+    await precachePicture(
+        ExactAssetPicture(
+            SvgPicture.svgStringDecoder, 'assets/icons/login.svg'),
+        null);
+    await precachePicture(
+        ExactAssetPicture(
+            SvgPicture.svgStringDecoder, 'assets/icons/signup.svg'),
+        null);
+    await precachePicture(
+        ExactAssetPicture(SvgPicture.svgStringDecoder, 'assets/icons/chat.svg'),
+        null);
+    return true;
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Auth',
-      theme: ThemeData(
-        primaryColor: kPrimaryColor,
-        scaffoldBackgroundColor: Colors.white,
-      ),
-      home: WelcomeScreen(),
+    return FutureBuilder<bool>(
+      builder: (context, projectSnap) {
+        if (projectSnap.data == true)
+          return  MaterialApp(
+                    debugShowCheckedModeBanner: false,
+                    title: 'Flutter Auth',
+                    theme: ThemeData(
+                      primaryColor: kPrimaryColor,
+                      scaffoldBackgroundColor: Colors.white,
+                    ),
+                    home: WelcomeScreen(),
+                  );
+        else {
+          return Text("Loading...");
+        }
+      },
+      future: loadPictures(),
     );
   }
 }
+
+
